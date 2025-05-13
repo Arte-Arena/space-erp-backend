895f84f9ec730a6b6ba304b6e12745111fad6d1c
Pablo Yuri <pabloyuridev@gmail.com>
2025-05-13 11:42:14 -0300

    improve create one handler

--- source/entities/leads/create_one.go
+++ source/entities/leads/create_one.go
@@ -1,7 +1,20 @@
 package leads
 
-import "net/http"
+import (
+	"api/source/schemas"
+	"api/source/utils"
+	"encoding/json"
+	"net/http"
+)
 
 func CreateOne(w http.ResponseWriter, r *http.Request) {
+	leadSchema := &schemas.Lead{}
+	if err := json.NewDecoder(r.Body).Decode(&leadSchema); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(schemas.ApiResponse{
+			Message: utils.SendInternalError(utils.LEADS_INVALID_REQUEST_DATA),
+		})
+		return
+	}
 
 }
