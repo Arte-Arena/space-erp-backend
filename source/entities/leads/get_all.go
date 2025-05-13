8105e36cacca5623b29ef55d89f3144d4ec9975d
Pablo Yuri <pabloyuridev@gmail.com>
2025-05-13 15:09:30 -0300

    Improve all leads handlers

--- source/entities/leads/get_all.go
+++ source/entities/leads/get_all.go
@@ -1,7 +1,60 @@
 package leads
 
-import "net/http"
+import (
+	"api/source/database"
+	"api/source/schemas"
+	"api/source/utils"
+	"context"
+	"encoding/json"
+	"net/http"
+	"os"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 func GetAll(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), database.MONGO_TIMEOUT)
+	defer cancel()
 
+	mongoURI := os.Getenv(utils.MONGODB_URI)
+	opts := options.Client().ApplyURI(mongoURI)
+	mongoClient, err := mongo.Connect(ctx, opts)
+	if err != nil {
+		w.WriteHeader(http.StatusBadGateway)
+		json.NewEncoder(w).Encode(schemas.ApiResponse{
+			Message: utils.SendInternalError(utils.CANNOT_CONNECT_TO_MONGODB),
+		})
+		return
+	}
+	defer mongoClient.Disconnect(ctx)
+
+	collection := mongoClient.Database(database.GetDB()).Collection(database.COLLECTION_LEADS)
+
+	filter := bson.D{}
+
+	cursor, err := collection.Find(ctx, filter)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(schemas.ApiResponse{
+			Message: utils.SendInternalError(utils.CANNOT_FIND_LEADS_IN_MONGODB),
+		})
+		return
+	}
+	defer cursor.Close(ctx)
+
+	leads := []schemas.Lead{}
+	if err := cursor.All(ctx, &leads); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(schemas.ApiResponse{
+			Message: utils.SendInternalError(utils.CANNOT_FIND_LEADS_IN_MONGODB),
+		})
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(schemas.ApiResponse{
+		Data: leads,
+	})
 }
--- source/entities/leads/get_one.go
+++ source/entities/leads/get_one.go
@@ -1,7 +1,70 @@
 package leads
 
-import "net/http"
+import (
+	"api/source/database"
+	"api/source/schemas"
+	"api/source/utils"
+	"context"
+	"encoding/json"
+	"net/http"
+	"os"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 func GetOne(w http.ResponseWriter, r *http.Request) {
+	idStr := r.PathValue("id")
 
+	id, err := primitive.ObjectIDFromHex(idStr)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(schemas.ApiResponse{
+			Message: utils.SendInternalError(utils.INVALID_LEAD_ID_FORMAT),
+		})
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), database.MONGO_TIMEOUT)
+	defer cancel()
+
+	mongoURI := os.Getenv(utils.MONGODB_URI)
+	opts := options.Client().ApplyURI(mongoURI)
+	mongoClient, err := mongo.Connect(ctx, opts)
+	if err != nil {
+		w.WriteHeader(http.StatusBadGateway)
+		json.NewEncoder(w).Encode(schemas.ApiResponse{
+			Message: utils.SendInternalError(utils.CANNOT_CONNECT_TO_MONGODB),
+		})
+		return
+	}
+	defer mongoClient.Disconnect(ctx)
+
+	collection := mongoClient.Database(database.GetDB()).Collection(database.COLLECTION_LEADS)
+
+	filter := bson.D{{Key: "_id", Value: id}}
+
+	lead := &schemas.Lead{}
+	err = collection.FindOne(ctx, filter).Decode(&lead)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			w.WriteHeader(http.StatusNotFound)
+			json.NewEncoder(w).Encode(schemas.ApiResponse{
+				Message: "Lead não encontrado",
+			})
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(schemas.ApiResponse{
+				Message: utils.SendInternalError(utils.CANNOT_FIND_LEAD_BY_ID_IN_MONGODB),
+			})
+		}
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(schemas.ApiResponse{
+		Data: lead,
+	})
 }
