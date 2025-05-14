851d40c504aa310cc6d9bc5b26510cac26963ed5
Pablo Yuri <pabloyuridev@gmail.com>
2025-05-14 10:25:57 -0300

    Add response handler

---
+++ source/utils/response_handler.go
@@ -0,0 +1,44 @@
+package utils
+
+import (
+	"api/source/schemas"
+	"encoding/json"
+	"net/http"
+)
+
+func SendResponse(w http.ResponseWriter, statusCode int, message string, data any, internalErrorCode int) {
+	if internalErrorCode != 0 {
+		w.WriteHeader(statusCode)
+		json.NewEncoder(w).Encode(schemas.ApiResponse{
+			Message: SendInternalError(internalErrorCode),
+		})
+		return
+	}
+
+	if (message == "") && (data == nil) {
+		w.WriteHeader(statusCode)
+		return
+	}
+
+	if (message != "") && (data == nil) {
+		w.WriteHeader(statusCode)
+		json.NewEncoder(w).Encode(schemas.ApiResponse{
+			Message: message,
+		})
+		return
+	}
+
+	if (message == "") && (data != nil) {
+		w.WriteHeader(statusCode)
+		json.NewEncoder(w).Encode(schemas.ApiResponse{
+			Data: data,
+		})
+		return
+	}
+
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(schemas.ApiResponse{
+		Data:    data,
+		Message: message,
+	})
+}
