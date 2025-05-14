b8e0264eca1cdd0a1aa9688e6765701490153ad0
Pablo Yuri <pabloyuridev@gmail.com>
2025-05-14 12:00:44 -0300

    Improve get by query handler

--- source/entities/report/get_by_query.go
+++ source/entities/report/get_by_query.go
@@ -37,16 +37,20 @@ func GetByQuery(w http.ResponseWriter, r *http.Request) {
 
 	_ = period
 
-	params.Get("clients_total")
-	params.Get("clients_monthly_average")
-	params.Get("clients_monthly_average")
-	params.Get("clients_conversion_less_thirty_days")
-	params.Get("clients_time_to_close_purchase")
-	params.Get("clients_new_per_month")
+	if reportType == schemas.REPORT_TYPE_CLIENTS {
+		params.Get("clients_total")
+		params.Get("clients_monthly_average")
+		params.Get("clients_monthly_average")
+		params.Get("clients_conversion_less_thirty_days")
+		params.Get("clients_time_to_close_purchase")
+		params.Get("clients_new_per_month")
+	}
 
-	params.Get("budgets_converted_sales")
-	params.Get("budgets_total_sales_value")
-	params.Get("budgets_average_ticket")
-	params.Get("budgets_monthly_sales_history")
-	params.Get("budgets_sales_value_by_segment")
+	if reportType == schemas.REPORT_TYPE_BUDGETS {
+		params.Get("budgets_converted_sales")
+		params.Get("budgets_total_sales_value")
+		params.Get("budgets_average_ticket")
+		params.Get("budgets_monthly_sales_history")
+		params.Get("budgets_sales_value_by_segment")
+	}
 }
