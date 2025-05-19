5bd67f6607a4aa7d8535e96e3b1b71a7ed8f0079
Pablo Yuri <pabloyuridev@gmail.com>
2025-05-19 16:25:35 -0300

    Improve get all handler

--- source/entities/leads/get_all.go
+++ source/entities/leads/get_all.go
@@ -5,8 +5,10 @@ import (
 	"api/source/schemas"
 	"api/source/utils"
 	"context"
+	"math"
 	"net/http"
 	"os"
+	"strconv"
 
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
@@ -16,6 +18,29 @@ import (
 func GetAll(w http.ResponseWriter, r *http.Request) {
 	ctx, cancel := context.WithTimeout(context.Background(), database.MONGO_TIMEOUT)
 	defer cancel()
+
+	pageStr := r.URL.Query().Get("page")
+	pageSizeStr := r.URL.Query().Get("pageSize")
+
+	page := int64(1)
+	pageSize := int64(25)
+
+	if pageStr != "" {
+		if parsedPage, err := strconv.ParseInt(pageStr, 10, 64); err == nil && parsedPage > 0 {
+			page = parsedPage
+		}
+	}
+
+	if pageSizeStr != "" {
+		if parsedPageSize, err := strconv.ParseInt(pageSizeStr, 10, 64); err == nil && parsedPageSize > 0 {
+			pageSize = parsedPageSize
+			if pageSize > 100 {
+				pageSize = 100
+			}
+		}
+	}
+
+	skip := (page - 1) * pageSize
 
 	mongoURI := os.Getenv(utils.MONGODB_URI)
 	opts := options.Client().ApplyURI(mongoURI)
@@ -30,7 +55,20 @@ func GetAll(w http.ResponseWriter, r *http.Request) {
 
 	filter := bson.D{}
 
-	cursor, err := collection.Find(ctx, filter)
+	totalItems, err := collection.CountDocuments(ctx, filter)
+	if err != nil {
+		utils.SendResponse(w, http.StatusInternalServerError, "", nil, utils.CANNOT_FIND_LEADS_IN_MONGODB)
+		return
+	}
+
+	totalPages := int64(math.Ceil(float64(totalItems) / float64(pageSize)))
+
+	findOptions := options.Find().
+		SetSkip(skip).
+		SetLimit(pageSize).
+		SetSort(bson.D{{Key: "created_at", Value: -1}})
+
+	cursor, err := collection.Find(ctx, filter, findOptions)
 	if err != nil {
 		utils.SendResponse(w, http.StatusInternalServerError, "", nil, utils.CANNOT_FIND_LEADS_IN_MONGODB)
 		return
@@ -43,5 +81,15 @@ func GetAll(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	utils.SendResponse(w, http.StatusOK, "", leads, 0)
+	response := map[string]any{
+		"items": leads,
+		"pagination": map[string]any{
+			"page":        page,
+			"page_size":   pageSize,
+			"total_items": totalItems,
+			"total_pages": totalPages,
+		},
+	}
+
+	utils.SendResponse(w, http.StatusOK, "", response, 0)
 }
