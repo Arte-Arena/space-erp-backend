2cdc7a7ce8046a00ffca4fe1b6e52dd6c716d1d1
Pablo Yuri <pabloyuridev@gmail.com>
2025-05-19 14:15:52 -0300

    Add users roles constants

--- source/schemas/users.go
+++ source/schemas/users.go
@@ -6,6 +6,18 @@ import (
 	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
+const (
+	USERS_ROLE_SUPER_ADMIN          = "super_admin"
+	USERS_ROLE_IT                   = "it"
+	USERS_ROLE_ADMIN                = "admin"
+	USERS_ROLE_LEADER               = "leader"
+	USERS_ROLE_COLLABORATOR         = "collaborator"
+	USERS_ROLE_DESIGNER             = "designer"
+	USERS_ROLE_DESIGNER_COORDINATOR = "designer_coordinator"
+	USERS_ROLE_PRODUCTION           = "production"
+	USERS_ROLE_COMMERCIAL           = "commercial"
+)
+
 type User struct {
 	ID        bson.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
 	OldID     uint64        `json:"old_id" bson:"old_id"`
