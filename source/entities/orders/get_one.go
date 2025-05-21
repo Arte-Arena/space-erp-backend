f1a48c23a60e19486cf503f00f9d017bd8ef54b1
Pablo Yuri <pabloyuridev@gmail.com>
2025-05-21 09:54:05 -0300

    Add new orders handlers

---
+++ source/entities/orders/get_one.go
@@ -0,0 +1,61 @@
+package orders
+
+import (
+	"api/source/database"
+	"api/source/utils"
+	"context"
+	"net/http"
+	"os"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+func GetOne(w http.ResponseWriter, r *http.Request) {
+	idStr := r.PathValue("id")
+
+	id, err := bson.ObjectIDFromHex(idStr)
+	if err != nil {
+		utils.SendResponse(w, http.StatusBadRequest, "", nil, utils.INVALID_ORDER_ID_FORMAT)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), database.MONGO_TIMEOUT)
+	defer cancel()
+
+	mongoURI := os.Getenv(utils.MONGODB_URI)
+	opts := options.Client().ApplyURI(mongoURI)
+	mongoClient, err := mongo.Connect(opts)
+	if err != nil {
+		utils.SendResponse(w, http.StatusBadGateway, "", nil, utils.CANNOT_CONNECT_TO_MONGODB)
+		return
+	}
+	defer mongoClient.Disconnect(ctx)
+
+	collection := mongoClient.Database(database.GetDB()).Collection(database.COLLECTION_ORDERS)
+
+	filter := bson.D{{Key: "_id", Value: id}}
+
+	var result bson.M
+	err = collection.FindOne(ctx, filter).Decode(&result)
+	if err == mongo.ErrNoDocuments {
+		utils.SendResponse(w, http.StatusNotFound, "Pedido não encontrado", nil, 0)
+		return
+	}
+	if err != nil {
+		utils.SendResponse(w, http.StatusInternalServerError, "", nil, utils.CANNOT_FIND_ORDER_BY_ID_IN_MONGODB)
+		return
+	}
+
+	if oldID, hasOldID := result["old_id"]; hasOldID {
+		if oldIDInt, canConvert := oldID.(int64); canConvert {
+			oldOrder, err := GetOneOld(int(oldIDInt))
+			if err == nil && oldOrder != nil {
+				result["old_data"] = oldOrder
+			}
+		}
+	}
+
+	utils.SendResponse(w, http.StatusOK, "", result, 0)
+}
