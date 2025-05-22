f73c7e056915461c85c8abeb4babb62368f96a98
Pablo Yuri <pabloyuridev@gmail.com>
2025-05-22 11:26:12 -0300

    Remove fetchs to get old data

--- source/entities/leads/get_one.go
+++ source/entities/leads/get_one.go
@@ -2,8 +2,6 @@ package leads
 
 import (
 	"api/source/database"
-	"api/source/entities/budgets"
-	"api/source/entities/orders"
 	"api/source/utils"
 	"context"
 	"net/http"
@@ -98,46 +96,6 @@ func GetOne(w http.ResponseWriter, r *http.Request) {
 	}
 
 	result := results[0]
-
-	if relatedOrders, ok := result["related_orders"].(bson.A); ok && len(relatedOrders) > 0 {
-		orderOldIDs := make([]int, 0)
-		for _, orderObj := range relatedOrders {
-			if orderMap, isMap := orderObj.(bson.M); isMap {
-				if oldID, hasOldID := orderMap["old_id"]; hasOldID {
-					if oldIDInt, canConvert := oldID.(int64); canConvert {
-						orderOldIDs = append(orderOldIDs, int(oldIDInt))
-					}
-				}
-			}
-		}
-
-		if len(orderOldIDs) > 0 {
-			oldOrders, err := orders.GetManyOld(orderOldIDs)
-			if err == nil && oldOrders != nil {
-				result["related_orders_old_data"] = oldOrders
-			}
-		}
-	}
-
-	if relatedBudgets, ok := result["related_budgets"].(bson.A); ok && len(relatedBudgets) > 0 {
-		budgetOldIDs := make([]int, 0)
-		for _, budgetObj := range relatedBudgets {
-			if budgetMap, isMap := budgetObj.(bson.M); isMap {
-				if oldID, hasOldID := budgetMap["old_id"]; hasOldID {
-					if oldIDInt, canConvert := oldID.(int64); canConvert {
-						budgetOldIDs = append(budgetOldIDs, int(oldIDInt))
-					}
-				}
-			}
-		}
-
-		if len(budgetOldIDs) > 0 {
-			oldBudgets, err := budgets.GetManyOld(budgetOldIDs)
-			if err == nil && oldBudgets != nil {
-				result["related_budgets_old_data"] = oldBudgets
-			}
-		}
-	}
 
 	utils.SendResponse(w, http.StatusOK, "", result, 0)
 }
