2bdddc207c3de58009c6b1eecee020ab27733827
Pablo Yuri <pabloyuridev@gmail.com>
2025-05-27 16:24:36 -0300

    Add laravel API url in init container

--- source/init-container.sh
+++ source/init-container.sh
@@ -5,6 +5,7 @@ echo "PORT=$PORT" >> .env
 echo "MONGODB_URI=$MONGODB_URI" >> .env
 echo "ENV=$ENV" >> .env
 echo "MYSQL_URI=$MYSQL_URI" >> .env
+echo "LARAVEL_API_URL=$LARAVEL_API_URL" >> .env
 
 
 echo "[arte arena security] Configurando variáveis de ambiente..."
