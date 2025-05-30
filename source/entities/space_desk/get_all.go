07cacbc8ada1a9008c66735ff3bdf3edd9ce2448
Pablo Yuri <pabloyuridev@gmail.com>
2025-05-30 11:50:42 -0300

    Add get all space desk handler initial configs

--- source/entities/space_desk/get_all.go
+++ source/entities/space_desk/get_all.go
@@ -1,1 +1,25 @@
 package spacedesk
+
+import (
+	"api/database"
+	"api/utils"
+	"context"
+	"net/http"
+)
+
+func GetAll(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), database.MONGO_TIMEOUT)
+	defer cancel()
+
+	typeParam := r.URL.Query().Get("type")
+	if typeParam == "messages" {
+		getMessages(w, r, ctx)
+		return
+	}
+
+	utils.SendResponse(w, http.StatusBadRequest, "", nil, utils.SPACE_DESK_INVALID_REQUEST_DATA)
+}
+
+func getMessages(w http.ResponseWriter, r *http.Request, ctx context.Context) {
+
+}
