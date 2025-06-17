dd9f97904dda0fbffda17bd4bc7601ca7b3c1b8f
Gustavo <gustavogarcia56336@gmail.com>
2025-06-17 12:33:07 -0300

    fix: cange chat_metadata struct

--- source/schemas/space_desk.go
+++ source/schemas/space_desk.go
@@ -160,18 +160,15 @@ type SpaceDeskMessageText struct {
 }
 
 type SpaceDeskChatMetadata struct {
-	ID                bson.ObjectID   `json:"_id" bson:"_id,omitempty"`
-	Name              string          `json:"name" bson:"name"`
-	NickName          string          `json:"nick_name" bson:"nick_name"`
-	ClientPhoneNumber string          `json:"cliente_phone_number" bson:"cliente_phone_number"`
-	Description       string          `json:"description" bson:"description"`
-	Status            string          `json:"status" bson:"status"`
-	Type              string          `json:"type" bson:"type"`
-	GroupIds          []bson.ObjectID `json:"group_ids" bson:"group_ids"`
-	UserId            string          `json:"user_id" bson:"user_id"`
-	CreatedAt         time.Time       `json:"created_at" bson:"created_at"`
-	UpdatedAt         time.Time       `json:"updated_at" bson:"updated_at"`
-	LastMessage       time.Time       `json:"last_message_timestamp" bson:"last_message_timestamp"`
+	ID                bson.ObjectID `json:"_id" bson:"_id,omitempty"`
+	Name              string        `json:"name" bson:"name"`
+	ClientPhoneNumber string        `json:"cliente_phone_number" bson:"cliente_phone_number"`
+	Status            string        `json:"status" bson:"status"`
+	Closed            bool          `json:"closed" bson:"closed"`
+	Need_template     bool          `json:"need_template" bson:"need_template"`
+	CreatedAt         time.Time     `json:"created_at" bson:"created_at"`
+	UpdatedAt         time.Time     `json:"updated_at" bson:"updated_at"`
+	LastMessage       time.Time     `json:"last_message_timestamp" bson:"last_message_timestamp"`
 }
 
 type Group struct {
