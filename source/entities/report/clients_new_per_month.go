7b2a5257857c4d289b0cf5f1434bf620d1f64acc
Pablo Yuri <pabloyuridev@gmail.com>
2025-06-30 16:35:23 -0300

    Add clients reports params functions

---
+++ source/entities/report/clients_new_per_month.go
@@ -0,0 +1,79 @@
+package report
+
+import (
+	"api/database"
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+func GetClientsNewPerMonth(from, until string) (map[string]int64, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), database.MONGO_TIMEOUT)
+	defer cancel()
+
+	mongoURI := os.Getenv("MONGODB_URI")
+	opts := options.Client().ApplyURI(mongoURI)
+	mongoClient, err := mongo.Connect(opts)
+	if err != nil {
+		return nil, err
+	}
+	defer mongoClient.Disconnect(ctx)
+
+	collection := mongoClient.Database(database.GetDB()).Collection(database.COLLECTION_CLIENTS)
+
+	filter := bson.D{}
+	if from != "" || until != "" {
+		dateFilter := bson.D{}
+		if from != "" {
+			if fromTime, err := time.Parse(time.RFC3339, from); err == nil {
+				dateFilter = append(dateFilter, bson.E{Key: "$gte", Value: fromTime})
+			}
+		}
+		if until != "" {
+			if untilTime, err := time.Parse(time.RFC3339, until); err == nil {
+				dateFilter = append(dateFilter, bson.E{Key: "$lte", Value: untilTime})
+			}
+		}
+		if len(dateFilter) > 0 {
+			filter = append(filter, bson.E{Key: "created_at", Value: dateFilter})
+		}
+	}
+
+	pipeline := bson.A{
+		bson.D{{Key: "$match", Value: filter}},
+		bson.D{{Key: "$group", Value: bson.D{
+			{Key: "_id", Value: bson.D{
+				{Key: "year", Value: bson.D{{Key: "$year", Value: "$created_at"}}},
+				{Key: "month", Value: bson.D{{Key: "$month", Value: "$created_at"}}},
+			}},
+			{Key: "count", Value: bson.D{{Key: "$sum", Value: 1}}},
+		}}},
+		bson.D{{Key: "$sort", Value: bson.D{{Key: "_id.year", Value: 1}, {Key: "_id.month", Value: 1}}}},
+	}
+
+	cursor, err := collection.Aggregate(ctx, pipeline)
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	result := make(map[string]int64)
+	for cursor.Next(ctx) {
+		var doc bson.M
+		if err := cursor.Decode(&doc); err != nil {
+			continue
+		}
+		id, _ := doc["_id"].(bson.M)
+		year, _ := id["year"].(int32)
+		month, _ := id["month"].(int32)
+		count, _ := doc["count"].(int32)
+		key := fmt.Sprintf("%04d-%02d", year, month)
+		result[key] = int64(count)
+	}
+	return result, nil
+}
