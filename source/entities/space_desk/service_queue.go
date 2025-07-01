dd4ef28592b08f413f3a15548fc6f9f37558b98c
Pablo Yuri <pabloyuridev@gmail.com>
2025-07-01 12:52:29 -0300

    Add service queue filter from query params function

--- source/entities/space_desk/service_queue.go
+++ source/entities/space_desk/service_queue.go
@@ -16,12 +16,33 @@ import (
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
+func buildServiceQueueFilterFromQueryParams(r *http.Request) bson.M {
+	query := r.URL.Query()
+	filter := bson.M{"closed": false}
+
+	if name := query.Get("name"); name != "" {
+		filter["name"] = bson.M{"$regex": name, "$options": "i"}
+	}
+
+	if number := query.Get("number"); number != "" {
+		filter["client_phone_number"] = bson.M{"$regex": number, "$options": "i"}
+	}
+
+	if untilStr := query.Get("until"); untilStr != "" {
+		if untilDays, err := strconv.Atoi(untilStr); err == nil && untilDays > 0 {
+			minTimestamp := time.Now().Add(-time.Duration(untilDays) * 24 * time.Hour).Unix()
+			filter["last_message_timestamp"] = bson.M{"$gte": time.Unix(minTimestamp, 0)}
+		}
+	}
+
+	return filter
+}
+
 func GetServiceQueue(w http.ResponseWriter, r *http.Request) {
 	ctx, cancel := context.WithTimeout(context.Background(), database.MONGO_TIMEOUT)
 	defer cancel()
 
 	query := r.URL.Query()
-	untilStr := query.Get("until")
 	limitStr := query.Get("limit")
 	pageStr := query.Get("page")
 
@@ -39,13 +60,6 @@ func GetServiceQueue(w http.ResponseWriter, r *http.Request) {
 	}
 	skip := (page - 1) * limit
 
-	minTimestamp := int64(0)
-	if untilStr != "" {
-		if untilDays, err := strconv.Atoi(untilStr); err == nil && untilDays > 0 {
-			minTimestamp = time.Now().Add(-time.Duration(untilDays) * 24 * time.Hour).Unix()
-		}
-	}
-
 	mongoURI := os.Getenv(utils.MONGODB_URI)
 	opts := options.Client().ApplyURI(mongoURI)
 	client, err := mongo.Connect(opts)
@@ -59,10 +73,7 @@ func GetServiceQueue(w http.ResponseWriter, r *http.Request) {
 	eventsCol := client.Database(database.GetDB()).Collection(database.COLLECTION_SPACE_DESK_EVENTS_WHATSAPP)
 
 	findOpts := options.Find().SetSort(bson.D{{Key: "last_message_timestamp", Value: 1}}).SetSkip(int64(skip)).SetLimit(int64(limit))
-	filter := bson.M{"closed": false}
-	if minTimestamp > 0 {
-		filter["last_message_timestamp"] = bson.M{"$gte": time.Unix(minTimestamp, 0)}
-	}
+	filter := buildServiceQueueFilterFromQueryParams(r)
 	cursor, err := chatCol.Find(ctx, filter, findOpts)
 	if err != nil {
 		utils.SendResponse(w, http.StatusInternalServerError, "", nil, utils.CANNOT_FIND_LEADS_IN_MONGODB)
