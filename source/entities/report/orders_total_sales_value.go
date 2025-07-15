e85b76f4c96fd1ad936284bf6e220a24ea48dc07
Pablo Yuri <pabloyuridev@gmail.com>
2025-07-15 10:45:07 -0300

    Add rders total sales values and monthly sales history

---
+++ source/entities/report/orders_total_sales_value.go
@@ -0,0 +1,108 @@
+package report
+
+import (
+	"api/database"
+	"context"
+	"encoding/json"
+	"os"
+	"strconv"
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+func GetOrdersTotalSalesValue(from, until string) (float64, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), database.MONGO_TIMEOUT)
+	defer cancel()
+
+	mongoURI := os.Getenv("MONGODB_URI")
+	opts := options.Client().ApplyURI(mongoURI)
+	mongoClient, err := mongo.Connect(opts)
+	if err != nil {
+		return 0, err
+	}
+	defer mongoClient.Disconnect(ctx)
+
+	collection := mongoClient.Database(database.GetDB()).Collection(database.COLLECTION_ORDERS)
+
+	filter := bson.D{}
+	if from != "" || until != "" {
+		dateFilter := bson.D{}
+		if from != "" {
+			if fromTime, err := time.Parse(time.RFC3339, from); err == nil {
+				dateFilter = append(dateFilter, bson.E{Key: "$gte", Value: fromTime})
+			}
+		}
+		if until != "" {
+			if untilTime, err := time.Parse(time.RFC3339, until); err == nil {
+				dateFilter = append(dateFilter, bson.E{Key: "$lte", Value: untilTime})
+			}
+		}
+		if len(dateFilter) > 0 {
+			filter = append(filter, bson.E{Key: "created_at", Value: dateFilter})
+		}
+	}
+
+	findOpts := options.Find().SetProjection(bson.M{
+		"products_list_legacy": 1,
+		"tiny.valor":           1,
+	})
+
+	cursor, err := collection.Find(ctx, filter, findOpts)
+	if err != nil {
+		return 0, err
+	}
+	defer cursor.Close(ctx)
+
+	type legacyProduct struct {
+		Preco      float64 `json:"preco"`
+		Quantidade float64 `json:"quantidade"`
+	}
+
+	var totalSales float64
+
+	for cursor.Next(ctx) {
+		var doc bson.M
+		if err := cursor.Decode(&doc); err != nil {
+			continue
+		}
+
+		var orderTotal float64
+
+		if listStr, _ := doc["products_list_legacy"].(string); listStr != "" {
+			var products []legacyProduct
+			if err := json.Unmarshal([]byte(listStr), &products); err == nil {
+				for _, p := range products {
+					qty := p.Quantidade
+					if qty == 0 {
+						qty = 1
+					}
+					orderTotal += p.Preco * qty
+				}
+			}
+		} else {
+			if tinyMap, ok := doc["tiny"].(bson.M); ok {
+				if valAny, ok2 := tinyMap["valor"]; ok2 {
+					switch v := valAny.(type) {
+					case float64:
+						orderTotal = v
+					case string:
+						if f, err := strconv.ParseFloat(v, 64); err == nil {
+							orderTotal = f
+						}
+					}
+				}
+			}
+		}
+
+		totalSales += orderTotal
+	}
+
+	if err := cursor.Err(); err != nil {
+		return 0, err
+	}
+
+	return totalSales, nil
+}
