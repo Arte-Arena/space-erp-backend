097eafc75203e9ce8b9395828f5d1994f74fe468
Gustavo <gustavogarcia56336@gmail.com>
2025-07-15 14:34:07 -0300

    feat: add routes for template and interactive pix message

--- source/main.go
+++ source/main.go
@@ -90,6 +90,9 @@ func main() {
 	mux.Handle("POST /v1/space-desk/chat", middlewares.LaravelAuth(http.HandlerFunc(spacedesk.CreateOneMessage)))
 	mux.Handle("POST /v1/space-desk/message", middlewares.LaravelAuth(http.HandlerFunc(spacedesk.CreateOneMessage)))
 	mux.Handle("POST /v1/space-desk/media", middlewares.LaravelAuth(http.HandlerFunc(spacedesk.CreateOneMedia)))
+	mux.Handle("POST /v1/space-desk/order-details", middlewares.LaravelAuth(http.HandlerFunc(spacedesk.CreateOrderDetails)))
+	mux.Handle("POST /v1/space-desk/order-template", middlewares.LaravelAuth(http.HandlerFunc(spacedesk.CreateOrderDetails)))
+	mux.Handle("POST /v1/space-desk/pix-message", middlewares.LaravelAuth(http.HandlerFunc(spacedesk.CreatePixMessage)))
 
 	mux.Handle("GET /v1/space-desk/media-base64", middlewares.LaravelAuth(http.HandlerFunc(spacedesk.HandlerMediaBase64)))
 	mux.Handle("GET /v1/space-desk/media-download", middlewares.LaravelAuth(http.HandlerFunc(spacedesk.HandlerMediaDownload)))
